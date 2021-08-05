b614cc94f44aea3cee084a57b6b24368ff9775ae
NetLSS <futureprogrammer@naver.com>
2021-08-05 12:38:05 +0900

    Chore: 현재 나의 위치 기능 추가 (플로팅 버튼)

--- app/src/main/java/com/lilcode/aop/p4c03/googlemap/MapActivity.kt
+++ app/src/main/java/com/lilcode/aop/p4c03/googlemap/MapActivity.kt
@@ -1,7 +1,17 @@
 package com.lilcode.aop.p4c03.googlemap
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,6 +20,7 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.lilcode.aop.p4c03.googlemap.databinding.ActivityMapBinding
+import com.lilcode.aop.p4c03.googlemap.model.LocationLatLngEntity
 import com.lilcode.aop.p4c03.googlemap.model.SearchResultEntity
 import java.lang.Exception
 
@@ -21,9 +32,14 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var searchResult: SearchResultEntity
 
+    private lateinit var locationManager: LocationManager // 안드로이드 에서 위치정보 불러올 때 관리해주는 유틸 클래스
+
+    private lateinit var myLocationListener: MyLocationListener // 나의 위치를 불러올 리스너
+
     companion object {
         const val SEARCH_RESULT_EXTRA_KEY: String = "SEARCH_RESULT_EXTRA_KEY"
         const val CAMERA_ZOOM_LEVEL = 17f
+        const val PERMISSION_REQUEST_CODE = 2021
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +56,17 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
             }
         }
 
+        bindViews()
     }
+
+    private fun bindViews() = with(binding){
+        // 현재 위치 버튼 리스너
+        currentLocationButton.setOnClickListener {
+            getMyLocation()
+        }
+    }
+
+
 
     private fun setupGoogleMap() {
         val mapFragment =
@@ -77,5 +103,104 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(positionLatLng, CAMERA_ZOOM_LEVEL))
 
         return map.addMarker(markerOptions)
+    }
+
+    private fun getMyLocation() {
+        // 위치 매니저 초기화
+        if(::locationManager.isInitialized.not()){
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
+
+        // GPS 이용 가능한지
+        val isGpsEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+        // 권한 얻기
+        if(isGpsEnable) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) { // 권한이 부여되지 않은 경우
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    PERMISSION_REQUEST_CODE
+                )
+            } else {
+                setMyLocationListener()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setMyLocationListener() {
+        val minTime = 1500L // 현재 위치를 불러오는데 기다릴 최소 시간
+        val minDistance = 100f // 최소 거리 허용
+
+        // 로케이션 리스너 초기화
+        if(::myLocationListener.isInitialized.not()) {
+            myLocationListener = MyLocationListener()
+        }
+
+        // 현재 위치 업데이트 요청
+        with(locationManager) {
+            requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime,
+                minDistance,
+                myLocationListener
+            )
+            requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                minTime,
+                minDistance,
+                myLocationListener
+            )
+        }
+    }
+
+    private fun onCurrentLocationChanged(locationLatLngEntity: LocationLatLngEntity) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+            LatLng(
+                locationLatLngEntity.latitude.toDouble(),
+                locationLatLngEntity.longitude.toDouble()
+        ), CAMERA_ZOOM_LEVEL))
+    }
+
+    // 권한 요청 결과 처리
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED &&grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    setMyLocationListener()
+                } else {
+                    Toast.makeText(this, "권한을 받지 못했습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    inner class MyLocationListener: LocationListener {
+        override fun onLocationChanged(location: Location) {
+            // 현재 위치 콜백
+            val locationLatLngEntity = LocationLatLngEntity(
+                location.latitude.toFloat(),
+                location.longitude.toFloat()
+            )
+
+            onCurrentLocationChanged(locationLatLngEntity)
+        }
+
     }
 }
